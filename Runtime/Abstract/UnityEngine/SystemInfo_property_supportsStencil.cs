713d699f31aab505262201393be6a3d8fb5dbd12
Serhii Fedorenko <fedorenkosergiy@gmail.com>
2022-04-02 21:05:13 +0300

    ˆ warnings

--- Runtime/Abstract/UnityEngine/SystemInfo_property_supportsStencil.cs
+++ Runtime/Abstract/UnityEngine/SystemInfo_property_supportsStencil.cs
@@ -5,7 +5,9 @@
 		/// <summary>
 		///   <para>Is the stencil buffer supported? (Read Only)</para>
 		/// </summary>
+		#if UNITY_5_5_OR_NEWER
 		[global::System.Obsolete("supportsStencil always returns true, no need to call it")]
+		#endif
 		public int supportsStencil { get; }
 	}
 }
--- Runtime/Abstract/UnityEngine/SystemInfo_property_supportsVertexPrograms.cs
+++ Runtime/Abstract/UnityEngine/SystemInfo_property_supportsVertexPrograms.cs
@@ -2,7 +2,9 @@
 {
 	public partial interface SystemInfo
 	{
+		#if UNITY_5_3_OR_NEWER
 		[global::System.Obsolete("Vertex program support is required in Unity 5.0+")]
+		#endif
 		public bool supportsVertexPrograms { get; }
 
 	}
