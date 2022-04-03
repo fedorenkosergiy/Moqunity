506763ce1c85e42b6b9add2e22a4f74973bf2553
Serhii Fedorenko <fedorenkosergiy@gmail.com>
2022-04-03 13:17:02 +0300

    - assemblies; + MonoBehaviour class

---
+++ Runtime/Abstract/UnityEngine/MonoBehaviour.cs
@@ -0,0 +1,9 @@
+namespace Moqunity.Abstract.UnityEngine
+{
+	/// <summary>
+	/// Some day it will be replaced by global using
+	/// </summary>
+	public abstract class MonoBehaviour : global::UnityEngine.MonoBehaviour
+	{
+	}
+}
