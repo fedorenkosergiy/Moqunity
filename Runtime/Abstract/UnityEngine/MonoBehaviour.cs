93e94fd24df8b5e55bdaf62cf73a4da971a14f2d
Serhii Fedorenko <fedorenkosergiy@gmail.com>
2022-04-03 15:56:38 +0300

    - mono behaviour

--- Runtime/Abstract/UnityEngine/MonoBehaviour.cs
+++
@@ -1,9 +0,0 @@
-namespace Moqunity.Abstract.UnityEngine
-{
-	/// <summary>
-	/// Some day it will be replaced by global using
-	/// </summary>
-	public abstract class MonoBehaviour : global::UnityEngine.MonoBehaviour
-	{
-	}
-}
