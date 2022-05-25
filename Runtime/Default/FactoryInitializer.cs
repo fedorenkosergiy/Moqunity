1ef3b0d92b587e7d767e15f853244eaf3d3b9be2
Serhii Fedorenko <fedorenkosergiy@gmail.com>
2022-05-26 02:48:09 +0300

    * prevent unneeded factory init

--- Runtime/Default/FactoryInitializer.cs
+++ Runtime/Default/FactoryInitializer.cs
@@ -3,9 +3,16 @@ namespace Moqunity
 	[global::UnityEngine.DefaultExecutionOrder(-10000)]
 	public class FactoryInitializer : global::UnityEngine.MonoBehaviour
 	{
+		private static bool _needInit = true;
+
 		private void Awake()
 		{
-			Context.Factory.Init();
+			if (_needInit)
+			{
+				Context.Factory.Init();
+				_needInit = false;
+			}
+
 			Destroy(this);
 		}
 	}
